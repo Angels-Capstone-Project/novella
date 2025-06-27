5059f6fc10cc573f68fe4da82d63c62a80e23823
Angel Oluwaseun Balogun <angel123@meta.com>
2025-06-27 15:49:20 -0700

    Changes

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "./utils/api.js";
-x;
+
 
 const genres = ["Romance", "Fantasy", "Mystery", "Sci-Fi", "Thriller"];
 
